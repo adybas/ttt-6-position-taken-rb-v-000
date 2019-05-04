034f28b3477d21073589751e4a493d1d132c25b3
Anna Dybas <a.m.dybas@gmail.com>
2019-05-04 21:53:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,14 @@
 # code your #position_taken? method here!
 =begin
+
 def position_taken?(board, index)
   if board[index] === "X" || board[index] === "O"
     true
   else
     false
   end
+end
+
 =end
 
 def position_taken?(board, index)
